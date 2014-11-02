a3ef0f5c3a2982f9671b5b4570bc78496c6bd433
erodriguezacamara <uriel.fmat@gmail.com>
2014-11-02 15:16:36 -0600

    Configuración para module/controller/action

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,44 @@
 <?php
-
+require_once '../library/Plugins/Layout.php';
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    protected function _initSession()
+    {
+    	Zend_Session::start();
+    }
+    
+    protected function _initLoaderResource()
+    {
+    	$resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+    			'basePath'  => APPLICATION_PATH . '/../application',
+    			'namespace' => 'Application'
+    	));
+    
+    	$modules = array("client");
+    
+    	foreach ($modules as $module){
+    		$autoLoader = new Zend_Application_Module_Autoloader(array(
+    				"namespace"=>ucfirst($module),
+    				"basePath"=>APPLICATION_PATH."/modules/".strtolower($module)
+    		));
+    	}
+    }
+    
+    protected function _initPlugins(){
+    	$this->bootstrap('frontController');
+    	$plugin = new Controller_Plugin_Layout();
+    	$this->frontController->registerPlugin($plugin);
+    }
+    
+    /*
+     protected function _initDoctype()
+     {
+    $this->bootstrap('view');
+    $view = $this->getResource('view');
+    $view->doctype('XHTML1_STRICT');
+    }
+    */
 
 }
 
---
+++ application/modules/client/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+class Client_IndexController extends Zend_Controller_Action {
+
+    public function preDispatch(){
+
+    }
+    
+	public function indexAction(){
+	  
+	}
+
+}

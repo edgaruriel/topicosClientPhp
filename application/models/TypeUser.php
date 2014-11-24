8350a2185fea11839e3772f2793f9c18cd82dfe8
Eddie <eddieparedes11@gmail.com>
2014-11-24 11:44:42 -0600

    Clases usuario y tipo usuario

---
+++ application/models/TypeUser.php
@@ -0,0 +1,39 @@
+<?php
+
+class Application_Model_TypeUser extends Application_Model_Abstract
+{
+    public $id;
+    public $name;
+    public static $typeUserArray = array("ADMIN"=>1,"CLIENT"=>2);
+
+
+	/**
+	 * @return the $name
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param field_type $name
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return the $id
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param field_type $id
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+}
+
+?>
---
+++ application/models/User.php
@@ -0,0 +1,71 @@
+<?php
+
+class Application_Model_User extends Application_Model_Abstract
+{
+    public $id;
+    public $userName;
+    public $password;
+    public $typeUser;
+	/**
+	 * @return the $id
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return the $password
+	 */
+	public function getPassword() {
+		return $this->password;
+	}
+
+	/**
+	 * @return the $typeUser
+	 */
+	public function getTypeUser() {
+		return $this->typeUser;
+	}
+
+	/**
+	 * @param field_type $id
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	/**
+	 * @param field_type $password
+	 */
+	public function setPassword($password) {
+		$this->password = $password;
+	}
+
+	/**
+	 * @param field_type $typeUser
+	 */
+	public function setTypeUser($typeUser) {
+		$this->typeUser = $typeUser;
+	}
+	
+	/**
+	 * @return the $userName
+	 */
+	public function getUserName() {
+		return $this->userName;
+	}
+
+	/**
+	 * @param field_type $userName
+	 */
+	public function setUserName($userName) {
+		$this->userName = $userName;
+	}
+
+
+    
+    
+    
+    
+}
+
